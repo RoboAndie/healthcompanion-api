d587826a8e11af5664a18bbbc380b1af189e3f03
Andrea Robertson <andrea.robertson25@gmail.com>
2017-07-09 12:25:47 -0500

    add validations

--- common/models/ailment.js
+++ common/models/ailment.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = function(Ailment) {
-
+    Ailment.validatesUniquenessOf('name', {message: 'Ailment name is not unique.'})
 };
--- common/models/medication.js
+++ common/models/medication.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = function(Medication) {
-
+    Medication.validatesInclusionOf('size', {in: ['Small', 'Medium', 'Large']});
+    Medication.validatesUniquenessOf('genericName', {message: 'Generic name is not unique.'})
 };
