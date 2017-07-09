e0b8b37cd3d6d20588f6ec6be1d6eff618c7cf16
Andrea Robertson <andrea.robertson25@gmail.com>
2017-07-08 21:34:13 -0500

    init

---
+++ app.js
@@ -0,0 +1,17 @@
+// /**
+//  * Module dependencies.
+//  */
+// const express = require('express');
+
+// /**
+//  * Create Express server.
+//  */
+// const app = express();
+
+// app.set('port', process.env.PORT || 3000);
+
+// app.listen(app.get('port'), () => {
+//   console.log('%s App is running at http://localhost:%d in %s mode', app.get('port'), app.get('env')); 
+//   console.log('  Press CTRL-C to stop\n');
+// });
+
